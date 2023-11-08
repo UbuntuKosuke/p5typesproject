ea76d83c32dec45a896b6a276bf802b629498d5f
akasataikisiti <nama0you0chu@gmail.com>
2023-11-09 01:44:50 +0900

    落下玉ちゃん

--- p5code/circle.ts
+++ p5code/circle.ts
@@ -1,18 +1,23 @@
 import p5 from "p5";
-import { canvasHeigh, canvasWidth } from "./main";
+import { canvasWidth } from "./main";
 
 export class Circle {
   private x: number;
   private y: number;
   private r: number;
+  private dy: number;
   constructor(private p5: p5) {
     this.x = this.p5.random(0, canvasWidth);
-    this.y = this.p5.random(0, canvasHeigh);
+    this.y = -50;
     this.r = this.p5.random(10, 20);
+    this.dy = 2;
   }
 
   drawCircle() {
     this.p5.fill(255, 0, 0, 100);
     this.p5.ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+  moveCircle() {
+    this.y += this.dy;
   }
 }
--- p5code/main.ts
+++ p5code/main.ts
@@ -7,19 +7,19 @@ export const canvasHeigh = 400;
 const canvas = document.querySelector("div#sketch") as HTMLElement;
 const sketch = (p: p5) => {
   const circles: Circle[] = [];
-  for (let i = 0; i < 10; i++) {
-    circles.push(new Circle(p));
-  }
 
   p.setup = () => {
     p.createCanvas(canvasWidth, canvasHeigh);
-    p.background(130);
   };
   p.draw = () => {
-    // if (p.floor(p.random(0, 10)) == 0) circles.push(new Circle(p));
+    if (p.floor(p.random(0, 10)) == 0) circles.push(new Circle(p));
 
+    p.background(130);
     // 描画処理など
-    circles.forEach((c) => c.drawCircle());
+    circles.forEach((c) => {
+      c.moveCircle();
+      c.drawCircle();
+    });
   };
 };
 new p5(sketch, canvas);
